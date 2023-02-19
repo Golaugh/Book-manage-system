d5b880ec7346c076fff9e55b3e83ac2989b9dd40
Golaugh <91037654+Golaugh@users.noreply.github.com>
2023-02-19 13:18:24 +0800

    Delete bookManageSystem directory

--- bookManageSystem/view/bookManageSystem/view/ueserdataImpl.java
+++
@@ -1,7 +0,0 @@
-package bookManageSystem.view;
-
-public class ueserdataImpl extends ueserdata {
-    ueserdataImpl(String xuehao, String name, String banji) {
-        super(xuehao, name, banji);
-    }
-}
